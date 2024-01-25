77794826313ca1d0cbb7824c326c7a7bcb322e58
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-01-25 01:19:15 +0000

    feat: Implemented SmartTvDevice.kt class

---
+++ src/main/kotlin/SmartTvDevice.kt
@@ -0,0 +1,27 @@
+class SmartTvDevice(deviceNmae: String, deviceCategory: String) :
+   SmartDevice(name = deviceNmae, category = deviceCategory) {
+
+    var speakerVolume = 2
+           set(value) {
+               if (value in 0..100) {
+                   field = value
+               }
+           }
+
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..200) {
+                field = value
+            }
+        }
+
+    fun increaseSpeakerVolume() {
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume.")
+    }
+
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
+    }
+}
