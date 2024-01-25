a66fd546b459705f8b72b0ada7cb429cf594ea79
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-01-25 00:56:02 +0000

    feat: Implemented properties inSmartDevice class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     val smartTvDevice = SmartDevice()
+    println("Device name is ${smartTvDevice.name}")
     smartTvDevice.turnOn()
     smartTvDevice.turnOff()
 }
